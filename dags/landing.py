e232881a432609a0b8eb01a5bf2509581ed6aa72
Hieu Nguyen <84280247+hellofromtheothersky@users.noreply.github.com>
2023-08-23 13:32:31 +0700

    add config check before run task
    just to python operator

--- dags/landing.py
+++ dags/landing.py
@@ -9,9 +9,15 @@ from airflow.hooks.base_hook import BaseHook
 
 from airflow.models.connection import Connection
 
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
 import pandas as pd
 
-def tables_to_csv_files(hook, tables_name):
+def tables_to_csv_files(hook):
+    tables_name=hook.get_records(sql="""	SELECT CONCAT(TABLE_SCHEMA, '.', TABLE_NAME)
+                                        FROM INFORMATION_SCHEMA.TABLES
+                                        WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG='BicycleRetailer' and TABLE_SCHEMA!='dbo'""")
     for table_name in tables_name:
         table_name=table_name[0]
         print('Table to csv for '+table_name)
@@ -19,25 +25,37 @@ def tables_to_csv_files(hook, tables_name):
         df = hook.get_pandas_df(sql=sql)
         df.to_csv('/opt/airflow/landing_cycle-sale/{0}.csv'.format(table_name), index=False)
 
+def able_to_run_task(task_name):
+    hook_config_table = MsSqlHook(mssql_conn_id='staging_db')
+    tuple=hook_config_table.get_records(sql="""
+                                        declare @able_to_run bit
+                                        EXEC able_to_run '{}', @able_to_run output
+                                        select @able_to_run
+                                        """.format(task_name))
+    return tuple[0][0]
+
 
 def landing_bicycle_retailer_db():
     # con=BaseHook.get_connection("source_bicycle_retailer_db").get_uri()
     # con+='?driver=ODBC+Driver+17+for+SQL+Server'
     # df =  pd.read_sql('SELECT * from [Production].[Product]', con)
     # print(df.head())
-    hook = MsSqlHook(mssql_conn_id='source_bicycle_retailer_db')
-    tables_name=hook.get_records(sql="""	SELECT CONCAT(TABLE_SCHEMA, '.', TABLE_NAME)
-	                                        FROM INFORMATION_SCHEMA.TABLES
-	                                        WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG='BicycleRetailer' and TABLE_SCHEMA!='dbo'""")
-    tables_to_csv_files(hook, tables_name)
+    can_run=able_to_run_task('landing_bicycle_retailer_db')
+    if can_run == False :
+        print("REFUSE TO RUN FROM CONFIG TABLE")
+    else:
+        hook = MsSqlHook(mssql_conn_id='source_bicycle_retailer_db')
+        # hook = MsSqlHook(mssql_conn_id='AAA')
+        tables_to_csv_files(hook)
 
 
 def landing_hrdb_db():
-    hook = MsSqlHook(mssql_conn_id='source_hrdb_db')
-    tables_name=hook.get_records(sql="""	SELECT CONCAT(TABLE_SCHEMA, '.', TABLE_NAME)
-	                                        FROM INFORMATION_SCHEMA.TABLES
-	                                        WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG='HRDB' and TABLE_SCHEMA!='dbo'""")
-    tables_to_csv_files(hook, tables_name)
+    can_run=able_to_run_task('landing_hrdb_db')
+    if can_run == False :
+        print("REFUSE TO RUN FROM CONFIG TABLE")
+    else:
+        hook = MsSqlHook(mssql_conn_id='source_hrdb_db')
+        tables_to_csv_files(hook)
 
 
 default_args = {
@@ -65,6 +83,7 @@ with DAG(
         bash_command="cp /opt/airflow/source/Csv/*.csv /opt/airflow/landing_cycle-sale"
     )
 
+    
     landing_excel = BashOperator(
         task_id='landing_excel',
         bash_command="cp /opt/airflow/source/Excel/*.xlsx /opt/airflow/landing_cycle-sale"
@@ -89,13 +108,13 @@ with DAG(
     landing_bicycle_retailer_db = PythonOperator(
         task_id='landing_bicycle_retailer_db',
         python_callable=landing_bicycle_retailer_db,
+
     )
 
     landing_hrdb_db = PythonOperator(
         task_id='landing_hrdb_db',
         python_callable=landing_hrdb_db,
     )
-
 
     end = BashOperator(
         task_id='end',
