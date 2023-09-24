8cb4e98ef4d4a36c55c30160a281a665461c79c5
Hieu <lycapower2002@gmail.com>
2023-09-24 22:01:34 +0700

    add load dim, and clarify the set config task calling

--- dags/load_dim.py
+++ dags/load_dim.py
@@ -18,22 +18,17 @@ from includes.control_table import update_task_runtime, load_enable_task_config
 from datetime import datetime, timedelta
 
 
-class LoadLandingToStagingArea(BaseOperator):
-    """
-    load file from landing area to staging in general
-    - with incremental: append data directly to the stage table
-    - with full: need an intermediate step to find changed data (by comparing the two data), and then do like incremental way
-    """
-    def __init__(self, target_conn_id, task_config, *args, **kwargs):
+class LoadStgToDim(BaseOperator):
+    def __init__(self, wh_conn_id, task_config, *args, **kwargs):
         super().__init__(*args, **kwargs) # initialize the parent operator
-        self.target_conn_id = target_conn_id
+        self.wh_conn_id = wh_conn_id
         self.task_config = task_config
 
     # execute() method that runs when a task uses this operator, make sure to include the 'context' kwarg.
     def execute(self, context):
         # write to Airflow task logs
-        hook = MsSqlHook(mssql_conn_id=self.target_conn_id)
-        hook.run("EXEC load_to_stage_table " + str(self.task_config['task_id']))
+        hook = MsSqlHook(mssql_conn_id=self.wh_conn_id)
+        hook.run("EXEC load_to_dim_scd2 " + str(self.task_config['task_id']))
 
 
 default_args = {
@@ -47,8 +42,8 @@ def format_yyyymmdd(datetime):
 
 with DAG( 
     default_args=default_args,
-    dag_id='staging',
-    description='staging',
+    dag_id='load_dim',
+    description='load_dim',
     start_date = datetime.now()- timedelta(days=2),
     schedule_interval='0 22 * * *',
     catchup=False,
@@ -62,17 +57,17 @@ with DAG(
         do_xcom_push=False,
     )
 
-    with TaskGroup(group_id='staging_gr') as staging_gr:
-        landing_test_db_db = LoadLandingToStagingArea.partial(
-            task_id='staging',
-            target_conn_id='warehouse_warehouse_db_db',
+    with TaskGroup(group_id='load_dim_gr') as load_dim_gr:
+        landing_test_db_db = LoadStgToDim.partial(
+            task_id='load_dim',
+            wh_conn_id='warehouse_warehouse_db_db',
             max_active_tis_per_dagrun=2,
-        ).expand(task_config=load_enable_task_config('staging'))
+        ).expand(task_config=load_enable_task_config('load_dim'))
 
     update_task_runtime = PythonOperator(
         task_id='update_task_runtime',
         python_callable=update_task_runtime,
-        op_kwargs={'dag_id': 'staging','load_cf_task_id': 'staging_gr.load_enable_task_config'},
+        op_kwargs={'dag_id': 'load_dim','load_cf_task_id': 'load_dim_gr.load_enable_task_config'},
         trigger_rule=TriggerRule.ALL_DONE,
     )
 
@@ -82,4 +77,4 @@ with DAG(
         do_xcom_push=False,
     )
 
-    start>>staging_gr>>update_task_runtime>>end
+    start>>load_dim_gr>>update_task_runtime>>end
